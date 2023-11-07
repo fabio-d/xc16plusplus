b658485996ebec67566337d38f138d2acd6af866
Dario Messina <nanodario@hotmail.co.uk>
2023-11-07 01:21:36 +0100

    Test 01-newdelete: Heap allocation produces invalid memory accesses in the C library (see XC16-1943)

--- autotests/01-newdelete/testdef.py
+++ autotests/01-newdelete/testdef.py
@@ -5,3 +5,11 @@ from testrun import *
 @register_test(omf=['elf', 'coff'])
 class MyTest(CompileAndRunTest):
     test_dir = os.path.abspath(os.path.dirname(__file__))
+
+    def compile(self, compilation_env):
+        # Heap allocation produces invalid memory accesses in the C library (see
+        # XC16-1943)
+        if (2, 0) <= compilation_env.compiler_version < (2, 10):
+            return CompilationOutput(Outcome.SKIPPED, dict())
+
+        return super().compile(compilation_env)
